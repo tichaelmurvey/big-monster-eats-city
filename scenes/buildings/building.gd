44036074be26a1d16b9845ac9da56c0a91ea12f4
SteamOS User <michaelturvey97@gmail.com>
2026-01-14 17:56:20 -0500

    damage to buildings, mushrooms bounce now

--- scenes/buildings/building.gd
+++ scenes/buildings/building.gd
@@ -1,17 +1,15 @@
 extends StaticBody2D
 @export var hit_points = 10
 @export var destroyed = false
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var destroyed_building = load("res://assets/building_destroyed.tres")
 
 func get_hit(dmg : float) -> void:
 	hit_points -= dmg
 	if hit_points <= 0:
 		hit_points = 0
 		destroyed = true
-		$Sprite2D.texture = "res://assets/building_destroyed.tres"
-		
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+		#TODO: Add more visual stages of building damage
+		$original.visible = false
+		$destroyed.visible = true
+		#removes self from all collision layers. IMPORTANT: this is bitmap, not integer of layer
+		collision_layer = 0
--- scenes/enemies/mushroom.gd
+++ scenes/enemies/mushroom.gd
@@ -1,20 +1,32 @@
 extends CharacterBody2D
 
-@export var SPEED = 30
+@export var SPEED = 150
+@export var ACCEL = 800
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	velocity = Vector2(0, -30)
+	velocity = Vector2(0, -10)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	handle_movement()
+	#update velocity (this is just for testing, pathfinding will take over this part!)
+	velocity.y += -ACCEL*delta
+	velocity = velocity.limit_length(SPEED)
+	
+	handle_movement(delta)
 
-func handle_movement() -> void:
-	var collision_info = move_and_collide(velocity)
+#Move enemy and register collisions
+func handle_movement(delta: float) -> void:
+	var collision_info = move_and_collide(velocity * delta)
 	if collision_info:
 		handle_collision(collision_info)
-		
+
+#handle collision
 func handle_collision(collision_info : KinematicCollision2D) -> void:
-	var collision_point = collision_info.get_position()
-	print(collision_point)
+	var hit_obj = collision_info.get_collider()
+	
+	#deal 1 damage to whatever it hit 
+	hit_obj.get_hit(1) 
+	# ^ nb this isn't very safe, trusts get_hit exists
+	#bounce off (TODO: post-hit dmg cooldown)
+	velocity = velocity.bounce(collision_info.get_normal())
