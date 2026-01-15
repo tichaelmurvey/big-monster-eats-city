39d7b49547a43d6a13e51f8444c02c5be3fe2a07
SteamOS User <michaelturvey97@gmail.com>
2026-01-15 12:55:36 -0500

    merge

---
+++ scenes/currency.gd
@@ -0,0 +1,13 @@
+extends Node2D
+@export var MONEY = 1000
+@export var CITIZENS = 1000
+@export var ENGINEERS = 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
